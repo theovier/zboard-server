f0fdfe01e93debfbba2d81293c27bd36881c3ec6
theovier <theo@harkenbusch.com>
2022-05-02 20:15:45 +0200

    add sanctum + auth scaffolding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,18 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+class DatabaseSeeder extends Seeder {
+    public function run() {
+        User::factory(10)->create();
     }
 }
