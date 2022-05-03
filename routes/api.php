42bc9e273323545c6cfe298df2e609eaafe4b0d1
theovier <theo@harkenbusch.com>
2022-05-03 09:40:31 +0200

    prepare for api authentication

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('test', function() {
-   return [
-       "hello" => "world"
-   ];
-});
+Route::post("login", [LoginController::class, "login"]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
