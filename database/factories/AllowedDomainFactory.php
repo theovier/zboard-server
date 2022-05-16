dbad894a15b0dc1914ce87202b11294e048bd065
theovier <theo@harkenbusch.com>
2022-05-16 20:08:06 +0200

    fix seeding bug that domains would violate unique constraints

--- database/factories/AllowedDomainFactory.php
+++ database/factories/AllowedDomainFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class AllowedDomainFactory extends Factory {
     public function definition() {
         return [
-            'name' => $this->faker->domainName(),
+            'name' => $this->faker->unique()->domainName(),
         ];
     }
 }
