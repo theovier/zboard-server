d5f550f02164fd654efc1a9b3bb3097e2e864561
theovier <theo@harkenbusch.com>
2022-05-18 10:26:20 +0200

    remove data-wrapper from all JsonResources as our frontend already has a data wrapper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider {
@@ -20,6 +21,7 @@ class AppServiceProvider extends ServiceProvider {
      * @return void
      */
     public function boot() {
-        //
+        //https://stackoverflow.com/a/64290670/6516194
+        JsonResource::withoutWrapping();
     }
 }
