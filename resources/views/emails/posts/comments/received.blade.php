14bf15fc7eccb499b4837baefc2ccdd1ecd4c339
theovier <theo@harkenbusch.com>
2022-05-19 16:13:04 +0200

    fix image aspect in email

--- resources/views/emails/posts/comments/received.blade.php
+++ resources/views/emails/posts/comments/received.blade.php
@@ -3,13 +3,13 @@
 
 ## You posted:
 
-<img src="{{$comment->post->author->profile_picture_url}}" style="width:75px; height: 75px; border-radius: 50%" alt="Your Profile Picture">
+<img src="{{$comment->post->author->profile_picture_url}}" style="object-fit: cover; width:75px; height: 75px; border-radius: 50%" alt="Your Profile Picture">
 
 > **{{ $comment->post->title }}** {{ $comment->post->content }}
 
 
 
-<img src="{{$comment->author->profile_picture_url}}" style="width:75px; height: 75px; border-radius: 50%" alt="Profile Picture Commentator">
+<img src="{{$comment->author->profile_picture_url}}" style="object-fit: cover; width:75px; height: 75px; border-radius: 50%;" alt="Profile Picture Commentator">
 
 ## {{ $comment->author->name }} ({{ $comment->author->email }}) replied:
 
