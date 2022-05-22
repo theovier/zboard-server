1449f7f4e4ed9c8beb9dc4af20f9a3f1765b6919
theovier <theo@harkenbusch.com>
2022-05-22 18:45:21 +0200

    fix connecting to docker container via shell script

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "Build Process started ..."
-
-# Pull the latest version
-git pull origin main
-
-# Connect to the Laravel docker container
-docker exec -it api /bin/bash
-
 # Clear all caches/routes/configs
 php artisan cache:clear
 php artisan config:clear
@@ -17,8 +9,3 @@ php artisan route:clear
 # Migrate and freshly seed the database.
 # Caution: this overwrites the whole database.
 php artisan migrate:fresh --seed
-
-# Leave the Docker container
-exit
-
-echo "Build Process finished!"
---
+++ .scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+echo "Deploy Process started ..."
+
+# Pull the latest version
+git pull origin main
+
+# Connect to the Laravel docker container
+docker exec -i api bash < build.sh
+
+echo "Deploy Process finished!"
